c2477179490c8fbfeff04b72efd46b7ee8f65a6b
Irvin H <irvinhomem@gmail.com>
2016-03-17 17:51:37 +0100

    Backing up

---
+++ test4.py
@@ -0,0 +1,60 @@
+from scapy.all import *
+import binascii as b2a
+
+def b32_8to5(a):
+    return "abcdefghijklmnopqrstuvwxyz012345".find(chr(a).lower())
+
+def up_header(p):
+    # print("Type p: ", type(p))
+    # print("P UP hex vals: ", p)
+    # print("P[0] int : ", hex(p[0]))
+    # print("P[1] int : ", int(p[1]))
+    return {
+        "userid": int(chr(p[0]), 16),
+        "up_seq": (b32_8to5(p[1]) >> 2) & 7,
+        "up_frag": ((b32_8to5(p[1]) & 3) << 2) | ((b32_8to5(p[2]) >> 3) & 3),
+        "dn_seq": (b32_8to5(p[2]) & 7),
+        "dn_frag": b32_8to5(p[3]) >> 1,
+        "lastfrag": b32_8to5(p[3]) & 1
+    }
+
+def dn_header(p):
+    # print("P DOWN hex vals: ", p)
+    # print("ord p[0]: ", ord(chr(p[0])))
+    # print("ord p[1]: ", ord(chr(p[1])))
+    return {
+        "compress": ord(chr(p[0])) >> 7,
+        "up_seq": (ord(chr(p[0])) >> 4) & 7,
+        "up_frag": ord(chr(p[0])) & 15,
+        "dn_seq": (ord(chr(p[1])) >> 1) & 15,
+        "dn_frag": (ord(chr(p[1])) >> 5) & 7,
+        "lastfrag": ord(chr(p[1])) & 1,
+   }
+
+p = rdpcap('bottle.cap')
+datasent = False
+for i in range(20,100):
+    if not p[i].haslayer(DNS):
+        continue
+    if DNSQR in p[i]:
+        if DNSRR in p[i] and len(p[i][DNSRR].rdata)>0: # downstream/server
+            d = p[i][DNSRR].rdata
+            hex_vals = str(b2a.hexlify(d[:30]))
+            formatted_hex =':'.join(hex_vals[j:j+2] for j in range(0, len(hex_vals), 2))
+            print("D[%i]3: %r" % (i, formatted_hex))
+            if datasent:
+                print("       %r" % dn_header(d))
+        else: # upstream/client
+            d = p[i][DNSQR].qname
+
+            hex_vals = str(b2a.hexlify(d[:30]))
+            formatted_hex =':'.join(hex_vals[j:j+2] for j in range(0, len(hex_vals), 2))
+            print("U[%i]3: %r" % (i, formatted_hex))
+
+            if str(chr(d[0])) in "0123456789abcdef":
+                # print('IN <-------')
+                print("       %r" % up_header(d))
+                datasent = True
+            else:
+                datasent = False
+
---
+++ test6_comb.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# Hack.lu 2010 CTF - Challenge #9 "Bottle"
+# Extract iodine DNS tunnel data
+# -- StalkR
+# Modified for Python3 --> irvinhomem
+# Using inbuilt zlib library
+from scapy.all import *
+from subprocess import Popen,PIPE
+import zlib as zl
+
+input, output = "bottle.cap", "extracted.cap"
+topdomain = b".pirate.sea."
+upstream_encoding = 128
+# and no downstream encoding (type NULL)
+
+# see encoder.c
+def encoder(base,encode="",decode=""): # base=[32,64,128]
+  p = Popen(["./encoder", str(base), "e" if len(encode)>0 else "d"], stdin=PIPE, stdout=PIPE)
+  p.stdin.write(encode if len(encode)>0 else decode)
+  return p.communicate()[0]
+
+def b32_8to5(a):
+  return "abcdefghijklmnopqrstuvwxyz012345".find(chr(a).lower())
+
+def up_header(p):
+  return {
+    "userid": int(chr(p[0]), 16),
+    "up_seq": (b32_8to5(p[1]) >> 2) & 7,
+    "up_frag": ((b32_8to5(p[1]) & 3) << 2) | ((b32_8to5(p[2]) >> 3) & 3),
+    "dn_seq": (b32_8to5(p[2]) & 7),
+    "dn_frag": b32_8to5(p[3]) >> 1,
+    "lastfrag": b32_8to5(p[3]) & 1
+  }
+
+def dn_header(p):
+  return {
+    "compress": ord(chr(p[0])) >> 7,
+    "up_seq": (ord(chr(p[0])) >> 4) & 7,
+    "up_frag": ord(chr(p[0])) & 15,
+    "dn_seq": (ord(chr(p[1])) >> 1) & 15,
+    "dn_frag": (ord(chr(p[1])) >> 5) & 7,
+    "lastfrag": ord(chr(p[1])) & 1,
+  }
+
+# Extract packets from DNS tunnel
+# Note: handles fragmentation, but not packet reordering (sequence numbers)
+p = rdpcap(input)
+dn_pkt, up_pkt = b'', b''
+datasent = False
+E = []
+for i in range(len(p)):
+  if not p[i].haslayer(DNS):
+    continue
+  if DNSQR in p[i]:
+    if DNSRR in p[i] and len(p[i][DNSRR].rdata)>0: # downstream/server
+      d = p[i][DNSRR].rdata
+      if datasent: # real data and no longer codec/fragment checks
+        dn_pkt += d[2:]
+        if dn_header(d)['lastfrag'] and len(dn_pkt)>0:
+          u = zl.decompress(dn_pkt)
+          if not u:
+            raise Exception("Error dn_pkt %i: %r" % (i, dn_pkt))
+          E += [IP(u[4:])]
+          dn_pkt = b''
+    else: # upstream/client
+      d = p[i][DNSQR].qname
+      if str(chr(d[0])).lower() in "0123456789abcdef":
+        datasent = True
+        up_pkt += d[5:-len(topdomain)].replace(b'.', b'')
+        if up_header(d)['lastfrag'] and len(up_pkt)>0:
+          u = zl.decompress(encoder(upstream_encoding, decode=up_pkt))
+          if not u:
+            raise Exception("Error up_pkt %i: %r" % (i, up_pkt))
+          E += [IP(u[4:])]
+          up_pkt = b''
+
+wrpcap(output, E)
+print("Successfully extracted %i packets into %s" % (len(E), output))
